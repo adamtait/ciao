73c36b0db19c7177a63e89fac9b36446ee1a7294
Adam Tait <adam@adamtait.com>
2018-03-09 14:01:21 -0800

    handle scripts being symbolically linked

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-source "_shared.sh"
+# In case this script is symbolically linked from elsewhere
+# Taken from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source "${SCRIPT_DIR}/_shared.sh"
+
+
+
 
 if [[ ! -d $D ]]; then
     echo "--- creating ${D}"
@@ -12,22 +24,12 @@ if [[ ! -f $F ]]; then
     touch $F
 fi
 
-if [[ ! -h $D/_shared.sh ]]; then
-    echo "--- linking _shared.sh to ${D}/_shared.sh"
-    ln -s _shared.sh $D/_shared.sh
-fi
-
-if [[ ! -h $D/trigger.sh ]]; then
-    echo "--- linking trigger.sh to ${D}/trigger.sh"
-    ln -s trigger.sh $D/trigger.sh
-fi
-
 
 
 if [[ $(command -v watchman) ]]; then
     echo "--- setup watchman on ${F}"
     watchman watch-project $D
-    watchman -- trigger $D update_etc_hosts -- $D/trigger.sh
+    watchman -- trigger $D update_etc_hosts -- $SCRIPT_DIR/trigger.sh
 else
     echo "--- watchman not installed. please see https://facebook.github.io/watchman for help."
 fi
