0a812ae47d5a7d1411ae1b2352c0b8a868f6313a
Adam Tait <adam@adamtait.com>
2018-03-10 09:37:13 -0800

    manage ON/OFF state better

--- cmd.sh
+++ cmd.sh
@@ -16,15 +16,17 @@ function block-domain () {
     if [[ "${1}" = "add" ]]; then
         echo "${2}" >> $F
     elif [[ "${1}" = "remove" ]]; then
-        sed '/${2}/d' $F > $F
+        sed -i '' '/${2}/d' $F
     elif [[ "${1}" = "on" ]]; then
+        touch $STATE
         $SCRIPT_DIR/trigger.sh
     elif [[ "${1}" = "off" ]]; then
-        cp $SRC_PATH $DEST_PATH
+        rm -rf $STATE
+        $SCRIPT_DIR/trigger.sh
     elif [[ "${1}" = "list" ]]; then
         cat $F
     else
-        echo "block-domain 1.0"
+        echo "block-domain 1.0.1"
         echo "manage a list of blocked domains. A blocked domain will not recieve any network requests (requests will be re-directed to 127.0.0.1)"
         echo "Usage: block-domain <command> [<args>]"
 
@@ -33,7 +35,7 @@ function block-domain () {
         echo -e "\tadd <domain>\tadd a new domain (like google.com) to the list of blocked domains"
         echo -e "\tremove <domain>\tremove domain from the list of blocked domains [NOTE: must exactly match previous entries]"
         echo -e "\ton\t\tstart blocking requests to stored internet domains"
-        echo -e "\toff\t\tstop blocking requests"
+        echo -e "\toff\t\tstop blocking requests. Restores /etc/hosts to original state."
                 
         echo -e "\nExamples:"
         echo -e "\tblock-domain add facebook.com"
