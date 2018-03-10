f5e76ee92f17fd5872e159e80fe1dee8aa474aa1
Adam Tait <adam@adamtait.com>
2018-03-10 09:38:07 -0800

    add information about editing /etc/sudoers to give necessary permissions
    + I wasn't able to find a way to replace /etc/hosts
      + using 666 permissions still gave an error
      + interestingly, appending to /etc/hosts succedeed when mv or cp failed

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 source "${SCRIPT_DIR}/_shared.sh"
 
 
+echo -e "\n########## DNS Blocker Setup: Complete ##########"
+
 
 
 if [[ ! -d $D ]]; then
@@ -43,3 +45,21 @@ else
     echo "--- watchman not installed. please see https://facebook.github.io/watchman for help."
 fi
 
+
+# INSTRUCTIONS FOR /etc/sudoers permissions
+echo -e "\n\n--------- IMPORTANT ---------"
+echo -e "\t For DNS Blocker to function, you need to give it sudo permissions"
+echo -e "\t Here's what you need to do:"
+echo -e "\n\t1. copy to following lines:"
+
+echo -e "\n## DNS Blocker (https://github.com/adamtait/DNS_blocker)"
+echo    "##   requires access to /etc/hosts"
+echo    "[user] ALL = NOPASSWD: ${SCRIPT_DIR}/trigger.sh"
+
+echo -e "\n\t 2. type: sudo visudo"
+echo -e "\t 3. once editor has loaded, append the copied lines to the bottom of the file"
+echo -e "\t 4. to save & quit, type 'ESC : w q'"
+echo -e "\n--------- FIN ---------"
+
+
+echo -e "\n\n########## DNS Blocker Setup: Complete ##########"
