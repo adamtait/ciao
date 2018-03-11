d680ab5d90e4d42b570d7e4a0d8ae95404d1d749
Adam Tait <adam@adamtait.com>
2018-03-10 17:56:20 -0800

    run trigger.sh on all changes
    + in process of removing references to watchman
    + bonus: cleaned up a bunch of the code

--- cmd.sh
+++ cmd.sh
@@ -1,47 +1,67 @@
 #!/usr/bin/env bash
 
-# In case this script is symbolically linked from elsewhere
-# Taken from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+get_script_dir () {
+    # In case this script is symbolically linked from elsewhere
+    # Taken from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+    SOURCE=$1
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    return "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+
+SCRIPT_DIR=get_script_dir("${BASH_SOURCE[0]}")
 source "${SCRIPT_DIR}/_shared.sh"
 
+HELP_TEXT="\
+$CMD $VERSION
+manage a list of blocked domains. A blocked domain will not recieve any network requests (requests will be re-directed to 127.0.0.1)
+Usage: $CMD <command> [<args>]
 
-function block-domain () {
-    if [[ "${1}" = "add" ]]; then
-        echo "${2}" >> $F
-    elif [[ "${1}" = "remove" ]]; then
-        sed -i '' '/${2}/d' $F
-    elif [[ "${1}" = "on" ]]; then
-        touch $STATE
-        $SCRIPT_DIR/trigger.sh
-    elif [[ "${1}" = "off" ]]; then
-        rm -rf $STATE
-        $SCRIPT_DIR/trigger.sh
-    elif [[ "${1}" = "list" ]]; then
-        cat $F
-    else
-        echo "block-domain 1.0.1"
-        echo "manage a list of blocked domains. A blocked domain will not recieve any network requests (requests will be re-directed to 127.0.0.1)"
-        echo "Usage: block-domain <command> [<args>]"
-
-        echo -e "\nAvailable commands are:"
-        echo -e "\tlist\t\tsee the list of blocked domains"
-        echo -e "\tadd <domain>\tadd a new domain (like google.com) to the list of blocked domains"
-        echo -e "\tremove <domain>\tremove domain from the list of blocked domains [NOTE: must exactly match previous entries]"
-        echo -e "\ton\t\tstart blocking requests to stored internet domains"
-        echo -e "\toff\t\tstop blocking requests. Restores /etc/hosts to original state."
+Available commands are:
+          list          see the list of blocked domains
+          add <domain>	add a new domain (like google.com) to the list of blocked domains
+          remove <domain>	remove domain from the list of blocked domains [NOTE: must exactly match previous entries]
+          on		start blocking requests to stored internet domains
+          off		stop blocking requests. Restores /etc/hosts to original state.
                 
-        echo -e "\nExamples:"
-        echo -e "\tblock-domain add facebook.com"
-        echo -e "\tblock-domain remove facebook.com"
-        echo -e "\tblock-domain off"
-        echo -e "\tblock-domain on"
-        echo -e "\nFor full documentation, see: https://github.com/adamtait/DNS_blocker#readme"
-    fi
+Examples:
+	$CMD add facebook.com
+	$CMD remove facebook.com
+	$CMD off
+	$CMD on
+
+For full documentation, see: https://github.com/adamtait/$CMD#readme"
+
+
+
+
+ciao () {
+    case "$1" in
+        list)
+            cat $F
+            exit 0
+            ;;
+        add)
+            echo "${2}" >> $F
+            ;;
+        remove)
+            sed -i '' '/${2}/d' $F
+            ;;
+        on)
+            touch $STATE
+            ;;
+        off)
+            rm -rf $STATE
+            ;;
+        *)
+            echo $HELP_TEXT
+            exit 0
+            ;;
+    esac
+
+    $SCRIPT_DIR/trigger.sh
 }
