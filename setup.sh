7085312da545266a348cb5a60a1ff3777cd56609
Adam Tait <adam@adamtait.com>
2018-03-10 17:57:55 -0800

    remove watchman

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # In case this script is symbolically linked from elsewhere
 # Taken from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 SOURCE="${BASH_SOURCE[0]}"
@@ -12,9 +13,9 @@ SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 source "${SCRIPT_DIR}/_shared.sh"
 
 
+
+
 echo -e "\n########## DNS Blocker Setup: Begin ##########"
-
-
 
 if [[ ! -d $D ]]; then
     echo "--- creating ${D}"
@@ -37,29 +38,4 @@ if [[ ! -f $STATE ]]; then
 fi
 
 
-if [[ $(command -v watchman) ]]; then
-    echo "--- setup watchman on ${F}"
-    watchman watch-project $D
-    watchman -- trigger $D update_etc_hosts -- $SCRIPT_DIR/trigger.sh
-else
-    echo "--- watchman not installed. please see https://facebook.github.io/watchman for help."
-fi
-
-
-# INSTRUCTIONS FOR /etc/sudoers permissions
-echo -e "\n\n\t--------- IMPORTANT ---------"
-echo -e "\t For DNS Blocker to function, you need to give it sudo permissions"
-echo -e "\t Here's what you need to do:"
-echo -e "\n\t1. copy to following lines:"
-
-echo -e "\n## DNS Blocker (https://github.com/adamtait/DNS_blocker)"
-echo    "##   requires access to /etc/hosts"
-echo    "[user] ALL = NOPASSWD: ${SCRIPT_DIR}/trigger.sh"
-
-echo -e "\n\t 2. type: sudo visudo"
-echo -e "\t 3. once editor has loaded, append the copied lines to the bottom of the file"
-echo -e "\t 4. to save & quit, type 'ESC : w q'"
-echo -e "\n\t--------- FIN ---------"
-
-
-echo -e "\n\n########## DNS Blocker Setup: Complete ##########\n"
+echo -e "\n########## DNS Blocker Setup: Fin ##########"
