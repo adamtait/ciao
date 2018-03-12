24a2f0eb5d928f921d333cf0c30eafe028d10e8e
Adam Tait <adam@adamtait.com>
2018-03-11 18:10:52 -0700

    make cmd.sh more readable

--- cmd.sh
+++ cmd.sh
@@ -17,28 +17,6 @@ export -f get_script_dir
 SCRIPT_DIR=$(get_script_dir "${BASH_SOURCE[0]}")
 source "${SCRIPT_DIR}/_shared.sh"
 
-HELP_TEXT="\
-$CMD $VERSION
-manage a list of blocked domains. A blocked domain will not recieve any network requests (requests will be re-directed to 127.0.0.1)
-Usage: $CMD <command> [<args>]
-
-Available commands are:
-          list                  see the list of blocked domains
-          add <domain>	        add a new domain (like google.com) to the list of blocked domains
-          remove <domain>	remove domain from the list of blocked domains [NOTE: must exactly match previous entries]
-          on		        start blocking requests to stored internet domains
-          off		        stop blocking requests. Restores /etc/hosts to original state.
-                
-Examples:
-	$CMD add facebook.com
-	$CMD remove facebook.com
-	$CMD off
-	$CMD on
-
-For full documentation, see: https://github.com/adamtait/$CMD#readme"
-
-
-
 
 ciao () {
     case "$1" in
@@ -66,3 +44,33 @@ ciao () {
 
     $SCRIPT_DIR/trigger.sh
 }
+
+
+
+
+HELP_TEXT="\
+$CMD $VERSION
+manage a list of blocked domains. A blocked domain will not recieve
+any network requests
+
+Usage: $CMD <command> [<args>]
+
+Available commands are:
+          list                  see the list of blocked domains
+          add <domain>	        add a new domain (like google.com) to the list of blocked domains
+          remove <domain>	remove domain from the list of blocked domains [NOTE: must exactly match previous entries]
+          on		        start blocking requests to stored internet domains
+          off		        stop blocking requests. Restores /etc/hosts to original state.
+                
+Examples:
+	$CMD add facebook.com
+	$CMD remove facebook.com
+	$CMD off
+	$CMD on
+
+NOTE! requests for added domains will be re-directed to: 
+IPv4 127.0.0.1
+IPV6 fe80::1%lo0
+
+For full documentation, see: https://github.com/adamtait/$CMD#readme"
+
