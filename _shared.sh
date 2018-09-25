09faf73ff20434247e9edb6b60f642a85727f5fb
Adam Tait <adam@adamtait.com>
2018-09-24 17:12:28 -0700

    load .ciao/config in _shared script

--- _shared.sh
+++ _shared.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-VERSION=1.0.1
+VERSION=1.0.2
 CMD=ciao
 
+# Paths
 D=$HOME/.ciao
 F=$D/domains
 STATE=$D/.state
 OVERRIDES=$D/overrides
 
 
+# Default Configuration
 SRC_PATH=$D/.etc_hosts~orig
 DEST_PATH=/etc/hosts
         
 IPv4_DEST="127.0.0.1"
 IPv6_DEST="fe80::1%lo0"
+
+DEST_DNS_FIRST=false
+
+# Local Configuration
+CONFIG_PATH=$D/config
+source $CONFIG_PATH
